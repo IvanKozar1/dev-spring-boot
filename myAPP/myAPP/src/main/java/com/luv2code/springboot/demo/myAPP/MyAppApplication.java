af5957da1bb9a45cec953225010a3be23ebf0594
IvanKozar1 <ivan.kozar3@gmail.com>
2025-02-05 12:29:38 +0100

    Prvi commit

---
+++ myAPP/myAPP/src/main/java/com/luv2code/springboot/demo/myAPP/MyAppApplication.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.demo.myAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+}
